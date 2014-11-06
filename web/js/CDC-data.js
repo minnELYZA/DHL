84837937ec122adfa62c6f748fe1682b6bb26644
jeanne.sim.2012 <jeanne.sim.2012@sis.smu.edu.sg>
2014-11-06 20:03:07 +0800

    Updated csv files in data folder - Change in certain RDC names as some names do not make sense

--- web/js/CDC-data.js
+++ web/js/CDC-data.js
@@ -1,3 +1,14 @@
-d3.csv("data/CDCSheet2.csv")
-    .row(function(d) { return {key: d.key, value: +d.value}; })
-    .get(function(error, rows) { console.log(rows); });
+d3.csv("data/CDCSheet2.csv", function (error, data) {
+    data.forEach(function (d) {
+        var cdc = d.CDC;
+        var carriers = d.Carriers;
+        var mostUtilisedCarriers = d.MostUtilisedCarrier;
+        var lessUtilisedCarrier = d.LessUtilisedCarrier;
+        var aveDensity  = d.AveDensity ;
+        var deliveries  = d.Deliveries ;
+        var aveCO2km  = d.AveCO2km ;
+        var trafficRating  = d.TrafficRating ;
+        var densityRating  = d.DensityRating ;
+        var efficiencyRating  = d.EfficiencyRating ;
+    });
+});
