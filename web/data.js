90057bef1c7a69aa2ecaea15f5abcd7ac26701cf
jeanne.sim.2012 <jeanne.sim.2012@sis.smu.edu.sg>
2014-11-13 01:57:58 +0800

    Renamed dcBarGraph.html to DHL.html added map.css, included codes for maps in DHL.html

--- web/data.js
+++ web/data.js
@@ -1,123 +1,137 @@
-            var dataTable = dc.dataTable("#dc-table-graph");
-            var RDCChart = dc.barChart("#dc-RDC-chart");
-            var DEChart = dc.barChart("#dc-DE-chart");
+var dataTable = dc.dataTable("#dc-table-graph");
+var RDCChart = dc.barChart("#dc-RDC-chart");
+var DEChart = dc.barChart("#dc-DE-chart");
+var CO2Chart = dc.barChart("#dc-CO2-chart");
 
-            d3.csv("data/RDCSheet2.csv", function (data) {
+// format our data
 
-                data.forEach(function (d) {
-                    d.AveDensity = Math.round(+d.AveDensity);
-                    d.Deliveries = +d.Deliveries;
-                    d.AveCO2km = (+d.AveCO2km).toFixed(3);
-                    d.TrafficRating = +d.TrafficRating;
-                    d.DensityRating = +d.DensityRating;
-                    d.EfficiencyRating = +d.EfficiencyRating;
-                });
+d3.csv("data/DataWithDates.csv", function (data) {
+    var dateFormat = d3.time.format("%m-%d-%Y").parse;
+    data.forEach(function (d) {
+        d.IndexDate = dateFormat(d.IndexDate);
 
+        d.Density = Math.round(+d.Density);
+        d.CO2 = Math.round(+d.CO2);
+        d.GCD = Math.round(+d.GCD);
+    });
+    console.log(data[0].IndexDate);
+    // Run the data through crossfilter and load our 'facts'
+    var facts = crossfilter(data);
+    var all = facts.groupAll();
 
-                // Run the data through crossfilter and load our 'facts'
-                var facts = crossfilter(data);
-                var all = facts.groupAll();
-
-                // Create avedensity value
-                var densityValue = facts.dimension(function (d) {
-                    return d.AveDensity;
-                });
-                var densityValueGroupCount = densityValue.group()
-                        .reduceCount(function (d) {
-                            return d.AveDensity;
-                        });
-                var densityValueGroupSum = densityValue.group()
-                        .reduceSum(function (d) {
-                            return d.AveDensity;
-                        });
-
-                // for traffic rating
-                var trValue = facts.dimension(function (d) {
-                    return d.TrafficRating;
-                });
-                var trValueGroup = trValue.group();
-
-                // Density rating
-                var deValue = facts.dimension(function (d) {
-                    return d.DensityRating;
-                });
-                var deValueGroup = deValue.group();
-
-                // Efficiency rating
-                var eValue = facts.dimension(function (d) {
-                    return d.EfficiencyRating;
-                });
-                var eValueGroup = eValue.group();
-
-                // Create dataTable dimension
-                var timeDimension = facts.dimension(function (d) {
-                    return d.AveDensity;
-                });
-
-                RDCChart.width(628)
-                        .height(150)
-                        .margins({top: 10, right: 10, bottom: 20, left: 40})
-                        .dimension(densityValue)
-                        .group(densityValueGroupCount)
-                        .transitionDuration(500)
-                        .centerBar(true)
-                        .gap(65)
-                        .filter([3, 5])
-                        .x(d3.scale.linear().domain(d3.extent(data, function (d) {
-                            return d.AveDensity;
-                        })))
-                        .elasticY(true)
-                        .xAxis().tickFormat();
-
-                DEChart.width(628)
-                        .height(150)
-                        .margins({top: 10, right: 10, bottom: 20, left: 40})
-                        .dimension(deValue)
-                        .group(deValueGroup)
-                        .transitionDuration(500)
-                        .centerBar(true)
-                        .gap(65)
-                        .x(d3.scale.linear().domain([0, 5]))
-                        .elasticY(true)
-                        .xAxis().tickFormat();
-
-                // Table of earthquake data
-                dataTable.width(960).height(800)
-                        .dimension(timeDimension)
-                        .group(function (d){
-                            return "<b><i>RDC Table</i></b>";
-                        })
-                        .size(10)
-                        .columns([
-                            function (d) {
-                                return d.AveDensity;
-                            },
-                            function (d) {
-                                return d.Deliveries;
-                            },
-                            function (d) {
-                                return d.AveCO2km;
-                            },
-                            function (d) {
-                                return d.TrafficRating;
-                            },
-                            function (d) {
-                                return d.DensityRating;
-                            },
-                            function (d) {
-                                return d.EfficiencyRating;
-                            }
-                        ])
-                        .sortBy(function (d) {
-                            return d.AveDensity;
-                        })
-                        .order(d3.ascending);
-
-                // Render the Charts
-
-
-                dc.renderAll();
-
-
+    // timechart
+    var deliveryByMonth = facts.dimension(function (d) {
+        return d.IndexDate;
+    });
+    var deliveryByMonthGroup = deliveryByMonth.group()
+            .reduceCount(function (d) {
+                return d.IndexDate;
             });
 
+    // Create density value
+    var densityValue = facts.dimension(function (d) {
+        return d.Density;
+    });
+    var densityValueGroupCount = densityValue.group()
+            .reduceCount(function (d) {
+                return d.Density;
+            });
+    var densityValueGroupSum = densityValue.group()
+            .reduceSum(function (d) {
+                return d.Density;
+            });
+    var co2Value = facts.dimension(function (d) {
+        return d.CO2;
+    });
+    var co2ValueGroupCount = co2Value.group()
+            .reduceCount(function (d) {
+                return d.CO2;
+            });
+    var co2ValueGroupSum = co2Value.group()
+            .reduceSum(function (d) {
+                return d.CO2;
+            });
+    var timeDimension = facts.dimension(function (d) {
+        return d.Density;
+    });
+
+    RDCChart.width(628)
+            .height(150)
+            .margins({top: 10, right: 10, bottom: 20, left: 20})
+            .dimension(deliveryByMonth)
+            .group(deliveryByMonthGroup)
+            .transitionDuration(500)
+            .centerBar(true)
+            .gap(65)
+            .filter([3, 5])
+            .x(d3.time.scale().domain(d3.extent(data, function (d) {
+                return d.IndexDate;
+            })))
+            .elasticY(true)
+            .xAxis().tickFormat();
+
+    DEChart.width(628)
+            .height(150)
+            .margins({top: 10, right: 10, bottom: 20, left: 20})
+            .dimension(densityValue)
+            .group(densityValueGroupCount)
+            .transitionDuration(500)
+            .centerBar(true)
+            .gap(65)
+            .x(d3.scale.linear().domain(d3.extent(data, function (d) {
+                return d.Density;
+            })))
+            .elasticY(true)
+            .xAxis().tickFormat();
+
+    CO2Chart.width(628)
+            .height(150)
+            .margins({top: 10, right: 10, bottom: 20, left: 20})
+            .dimension(co2Value)
+            .group(co2ValueGroupCount)
+            .transitionDuration(500)
+            .centerBar(true)
+            .gap(65)
+            .x(d3.scale.linear().domain([0, 5000]))
+            .y(d3.scale.linear().domain([15, 20]))
+            .elasticY(true)
+            .xAxis().tickFormat();
+
+    // Table of earthquake data
+    dataTable.width(1288).height(800)
+            .dimension(timeDimension)
+            .group(function (d) {
+                return "<b><i>RDC Table</i></b>";
+            })
+            .size(1000)
+            .columns([
+                function (d) {
+                    return d.Density;
+                },
+                function (d) {
+                    return d.GCD;
+                },
+                function (d) {
+                    return d.CO2;
+                },
+                function (d) {
+                    return d.IndexDate.toString().substring(4, 15);
+                },
+                function (d) {
+                    return d.EnglishOrigin;
+                },
+                function (d) {
+                    return d.EnglishDestination;
+                },
+                function (d) {
+                    return d.CO2Metric;
+                }
+            ])
+            .sortBy(function (d) {
+                return d.Density;
+            })
+            .order(d3.ascending);
+
+    // Render the Charts
+    dc.renderAll();
+});
