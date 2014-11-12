f0947f6554a09be22dce0ec2a336ff88b725e7ca
Elyza <mingee.yao.2012@sis.smu.edu.sg>
2014-11-12 20:40:02 +0800

    Added v rough version of routes
    to be improved

--- web/js/routes.js
+++ web/js/routes.js
@@ -4,14 +4,19 @@
  * and open the template in the editor.
  */
 
+var graticule = d3.geo.graticule();
+  
+var svg = d3.select("svg").select("g");
+
+var cdc = {};
+var rdc = {};
+
 d3.csv("./data/Addresses.csv", function(error, addresses) {
   addresses.forEach(function (d){
     d.Latitude = +d.Latitude;
     d.Longitude = +d.Longitude;
   });
   
-  var cdc = {};
-  var rdc = {};
   for (add in addresses) {
     if (addresses[add].Type === "CDC") {
       cdc[addresses[add].Name] = [addresses[add].Longitude, addresses[add].Latitude];
@@ -19,16 +24,12 @@ d3.csv("./data/Addresses.csv", function(error, addresses) {
       rdc[addresses[add].Name] = [addresses[add].Longitude, addresses[add].Latitude];
     }
   }
-  
-  var graticule = d3.geo.graticule();
-  
-  var svg = d3.select("svg").select("g");
-  
+
   svg.append("path")
     .datum(graticule)
     .attr("class", "graticule")
     .attr("d", path);
-  
+
   var ptCdc = svg.append("g")
     .attr("class", "points cdc")
     .selectAll("g")
@@ -43,7 +44,7 @@ d3.csv("./data/Addresses.csv", function(error, addresses) {
     .attr("y", -25)
     .attr("dy", ".71em")
     .text(function(d) { return d.key; });
-    
+
   var ptRdc = svg.append("g")
     .attr("class", "points rdc")
     .selectAll("g")
@@ -58,5 +59,36 @@ d3.csv("./data/Addresses.csv", function(error, addresses) {
     .attr("y", 10)
     .attr("dy", ".71em")
     .text(function(d) { return d.key; });
+    
+    
+    
+    
+  d3.csv("./data/RouteSheet3.csv", function(error, route) {
+  var arc = {};
+  for (r in route) {
+    var name = route[r].RouteName;
+    var name1 = name.substr(0,name.lastIndexOf(",")).trim();
+    var name2 = name.substr(name.lastIndexOf(",") + 1).trim();
+    // look to remove arc later
+    arc[name1] = cdc[name1];
+    arc[name2] = rdc[name2];
+    
+    var line = {
+      type: "LineString",
+      coordinates: [
+        cdc[name1],
+        rdc[name2]
+      ]
+    };
+    
+    svg.append("path")
+    .datum(line)
+    .attr("class", "route")
+    .attr("d", path);
+    
+    arc = {};
+  }
+  
+});
 });
 
